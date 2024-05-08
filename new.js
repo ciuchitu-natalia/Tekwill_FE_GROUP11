cef7ef9d9e9c96671f34ce30ee78df7fdf6d1474
ciuchitu-natalia <tusea-tolik@mail.ru>
2024-05-08 15:51:14 +0300

    rezolv conflict

---
+++ new.js
@@ -0,0 +1,15 @@
+var country = "island Great";
+continent = "Europa";
+population = 0.039 ;
+ 
+console.log ("country-", country);
+console.log ("continent-", continent);
+console.log ("population-", population,"milion." );
+
+var island = (country=="island")
+console.log("island-", island );
+var countryDiscription =`${country} is located in ${continent}, ${population} milion people live here`;
+ 
+console.log ("Country Discription:", countryDiscription );
+
+
